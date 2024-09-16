e85a238b7bef0919e121edf11d118050905de7a7
pthanhtung412 <pthanhtung412@gmail.com>
2024-09-16 17:04:42 +0700

    commit

---
+++ KMIN_project/src/components/Layouts/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header_content">
+                <img src="./assets/images/MinDev_logo.png" alt="" />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
