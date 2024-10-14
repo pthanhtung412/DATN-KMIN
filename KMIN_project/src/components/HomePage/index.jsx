cd97330e72e533b57f64cec8944988cff3526aa5
pthanhtung412 <pthanhtung412@gmail.com>
2024-10-14 16:52:56 +0700

    commit 14/10

--- KMIN_project/src/components/HomePage/index.jsx
+++ KMIN_project/src/components/HomePage/index.jsx
@@ -4,7 +4,31 @@ import styles from "./HomePage.module.css";
 const Home = () => {
   return (
     <>
-      <div className={clsx(styles.loginForm)}></div>
+      <div className={clsx(styles.homePage)}>
+        <div className={clsx(styles.leftNavbar)}>
+          <div className={clsx(styles.divRow)} style={{ color: '#808080', fontSize: '14px'}}>
+            MENU
+          </div>
+          <div className={clsx(styles.divRow)}>
+            Questions
+          </div>
+          <div className={clsx(styles.divRow)}>
+            Tags
+          </div>
+          <div className={clsx(styles.divRow)} style={{ color: '#808080', marginTop: '15px', fontSize: '14px'}}>
+            PERSONAL NAVIGATOR
+          </div>
+          <div className={clsx(styles.divRow)}>
+            Your questions
+          </div>
+          <div className={clsx(styles.divRow)}>
+            Your answers
+          </div>
+          <div className={clsx(styles.divRow)}>
+            Your likes & votes
+          </div>
+        </div>
+      </div>
     </>
   );
 };
