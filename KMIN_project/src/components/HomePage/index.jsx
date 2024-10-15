c1528a486bffdc6cd5346355ff3306377c3219a7
pthanhtung412 <pthanhtung412@gmail.com>
2024-10-15 13:59:42 +0700

    15/10/01

--- KMIN_project/src/components/HomePage/index.jsx
+++ KMIN_project/src/components/HomePage/index.jsx
@@ -10,21 +10,26 @@ const Home = () => {
             MENU
           </div>
           <div className={clsx(styles.divRow)}>
+            <img src="/assets/images/list.png" alt="list.png" />
             Questions
           </div>
           <div className={clsx(styles.divRow)}>
+            <img src="/assets/images/tag.png" alt="tag.png" />
             Tags
           </div>
           <div className={clsx(styles.divRow)} style={{ color: '#808080', marginTop: '15px', fontSize: '14px'}}>
             PERSONAL NAVIGATOR
           </div>
           <div className={clsx(styles.divRow)}>
+            <img src="/assets/images/help-circle.png" alt="help-circle.png" />
             Your questions
           </div>
           <div className={clsx(styles.divRow)}>
+            <img src="/assets/images/message-circle.png" alt="message-circle.png" />
             Your answers
           </div>
           <div className={clsx(styles.divRow)}>
+            <img src="/assets/images/heart.png" alt="heart.png" />
             Your likes & votes
           </div>
         </div>
